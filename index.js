ca913cd50dde6354984119365e471db2fa42e5e9
Srinivasa Y R <VFG3587@VFG-DC.LOCAL>
2026-05-19 12:54:12 +0530

    second

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require('express')
 let app=express();
 
 app.get('/',(req,res)=>{
-    res.json({"message":"Server is up and running v4 automated by github actions"})
+    res.json({"message":"Server is up and running v5 automated by github actions"})
 })
 
 app.listen(5000,()=>{
