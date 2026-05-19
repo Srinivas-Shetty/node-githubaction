b94f1412891dd74f88105958ef6c96920cd3b83e
Srinivasa Y R <VFG3587@VFG-DC.LOCAL>
2026-05-19 11:51:52 +0530

    third

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require('express')
 let app=express();
 
 app.get('/',(req,res)=>{
-    res.json({"message":"Server is up and running v2"})
+    res.json({"message":"Server is up and running v3"})
 })
 
 app.listen(5000,()=>{
